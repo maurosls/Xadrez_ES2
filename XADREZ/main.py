44ebd0fc46d0d6d569ef15a15f08a14c6e00aabb
leandro-az <leandro_almeida@id.uff.br>
2017-05-20 18:17:27 -0300

    alteracao na main agora com interface grafica

--- XADREZ/main.py
+++ XADREZ/main.py
@@ -1,4 +1,52 @@
 from tabuleiro import *
+from PPlay.window import *
+from PPlay.sprite import *
+
+
+janela = Window(800, 600)
+resp = True
+mouse =  janela.get_mouse()
+
+botao1j = Sprite("Sprites/botao1j.png")
+botao1j.x= 100
+botao1j.y= 200
+
+botao2j = Sprite("Sprites/botao2j.png")
+botao2j.x= 100
+botao2j.y= 300
+
+botaopc = Sprite("Sprites/botaopc.png")
+botaopc.x= 100
+botaopc.y= 400
+
+botaoreg = Sprite("Sprites/botaoreg.png")
+botaoreg.x= 100
+botaoreg.y= 500
+
+botaotit = Sprite("Sprites/nomej.png")
+botaotit.x= 100
+botaotit.y= 80
+
+imagemtab = Sprite("Sprites/imagemtab.png")
+imagemtab.x= 350
+imagemtab.y= 330
+
+
+while resp:
+    janela.update()
+    botao1j.draw()
+    botao2j.draw()
+    botaopc.draw()
+    botaoreg.draw()
+    botaotit.draw()
+    imagemtab.draw()
+    if(mouse.is_button_pressed(1) and mouse.is_over_object(botao1j) or
+        mouse.is_button_pressed(1) and mouse.is_over_object(botao2j) or
+        mouse.is_button_pressed(1) and mouse.is_over_object(botaopc)):
+        resp = False
+        janela.delay(500)
+
+
 
 tab = Tabuleiro()
 
