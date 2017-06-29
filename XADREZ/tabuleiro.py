1ce58e713e18594600d884716f286fda600c98e2
Deflaimun <paulohtb@hotmail.com>
2017-06-29 13:49:29 -0300

    removendo casa dos mortos

--- XADREZ/tabuleiro.py
+++ XADREZ/tabuleiro.py
@@ -20,10 +20,6 @@ class Tabuleiro:
     corCheck = None
     checkM = False
     corCheckM = None
-    pretasComidas = []
-    brancasComidas = []
-    casaBrancosMortos = []
-    casaPretosMortos = []
     casas = None
     selecao = None
 
@@ -93,36 +89,10 @@ class Tabuleiro:
             else:
                 cor = "preto"
         self.rodada = "branco"
-
-#Inicializaçao das casas dos mortos-------------------------------------------------------------------------------------------------------------------------------------------------
-        cor = "preto"
-        for i in range (0, 16):
-            if (cor == "preto"):
-                morto1 = Sprite("Sprites/morto-1.png")
-                morto2 = Sprite("Sprites/morto-2.png")
-                cor = "branco"
-            else:
-                morto1 = Sprite("Sprites/morto-2.png")
-                morto2 = Sprite("Sprites/morto-1.png")
-                cor = "preto"
-            if( i < 8):
-                morto1.x = 0
-                morto2.x = 750
-                index = i
-            else:
-                temp = morto1
-                morto1 = morto2
-                morto2 = temp
-                morto1.x = 75
-                morto2.x = 825
-                index = i - 8
-            morto1.y = index * self.tamanhoSprite
-            morto2.y = index * self.tamanhoSprite
-            self.casaBrancosMortos.append(morto1)
-            self.casaPretosMortos.append(morto2)
         self.selecaoSprite = Sprite("Sprites/selecao.png")
         self.selecaoSprite.x = self.janela.width
         self.selecaoSprite.y = self.janela.height
+
 
 # método para verificar o tempo do jogo ---------------------------------------------------------------------------------------------------------
 
@@ -573,27 +543,8 @@ class Tabuleiro:
             else:
                 self.estadoPromocaoPeao = False
 
-
-    def moveOMorto(self, acao):
-        if(self.matriz[acao.linha][acao.coluna].cor == "branco"):
-            self.brancasComidas.append(self.matriz[acao.linha][acao.coluna])
-            index = len(self.brancasComidas)-1      
-            if(index < 8):
-                self.matriz[acao.linha][acao.coluna].sprite.x = 0
-            else:
-                self.matriz[acao.linha][acao.coluna].sprite.x = 75
-                index = index - 8
-        if(self.matriz[acao.linha][acao.coluna].cor == "preto"):
-            self.pretasComidas.append(self.matriz[acao.linha][acao.coluna])
-            index = len(self.pretasComidas)-1
-            if(index < 8):
-                self.matriz[acao.linha][acao.coluna].sprite.x = 750
-            else:
-                self.matriz[acao.linha][acao.coluna].sprite.x = 825
-                index = index - 8
-        self.matriz[acao.linha][acao.coluna].sprite.y = 75 * index
-
     def limpaDisponíveis(self):
+
         for x in range(0, len(self.matriz)):
             for y in range(0, len(self.matriz[0])):
                 if (self.matriz[x][y] != "vazio"):
@@ -632,7 +583,7 @@ class Tabuleiro:
                     self.selecao.jaMoveu = True
 
             elif(updateAlvo==True):
-                self.moveOMorto(acao)
+           #     self.moveOMorto(acao)
                 self.matriz[acao.linha][acao.coluna].alvo = False
                 self.matriz[self.selecao.linha][self.selecao.coluna] = "vazio"
                 self.selecao.linha = acao.linha
@@ -643,7 +594,7 @@ class Tabuleiro:
             self.janela.update()
             self.desenha_base_do_Tabuleiro()
             self.desenhaMatSprites()
-            self.desenhaMat_de_Mortos()
+           # self.desenhaMat_de_Mortos()
             self.janela.update()
             self.verificaPromocao(self.matriz[acao.linha][acao.coluna])
 
@@ -691,15 +642,6 @@ class Tabuleiro:
                     self.matriz[i][j].sprite.y = i * self.tamanhoSprite
                     self.matriz[i][j].sprite.draw()
 
-    def desenhaMat_de_Mortos(self):
-        for i in range(0, len(self.casaBrancosMortos)):
-            self.casaBrancosMortos[i].draw()
-        for i in range(0, len(self.casaPretosMortos)):
-            self.casaPretosMortos[i].draw()
-        for i in range(0, len(self.brancasComidas)):
-            self.brancasComidas[i].sprite.draw()
-        for i in range(0, len(self.pretasComidas)):
-            self.pretasComidas[i].sprite.draw()
 
     def desenha_base_do_Tabuleiro(self):
         for i in range(0, len(self.casas)):
@@ -806,7 +748,7 @@ class Tabuleiro:
 
         self.desenha_base_do_Tabuleiro()
 
-        self.desenhaMat_de_Mortos()
+        #self.desenhaMat_de_Mortos()
 
         for i in range(0,len(self.matriz)):
             for j in range(0,len(self.matriz[0])):
